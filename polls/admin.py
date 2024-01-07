e9cb8eae8c0402788bf8a373d26db70d11163329
Edson Heiji Kato Junior <katojuniorbgi@gmail.com>
2024-01-07 12:44:29 +0900

    add new page

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Choice, Question
 
+from django.urls import path
+from django.http import HttpResponse
+from django.shortcuts import render
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
-
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -16,6 +18,21 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']  
     search_fields = ['question_text'] 
+    
+    # Método para ação personalizada
+    def new_page_view(self, request):
+        url_referencia = request.META.get('HTTP_REFERER')
+        return render(request, 'admin/new_page.html', {'url_referencia': url_referencia} )
 
+    # Método para adicionar URL personalizada
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('new_page/', self.admin_site.admin_view(self.new_page_view), name='new_page'),
+        ]
+        return custom_urls + urls
+
+
+    
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -25,15 +25,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text    
-    
-
-
-
-
-
-
-
-
-
-
-
